7d08cf48307eee3556f8787e290ee2a0abebe84f
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2025-02-15 13:16:35 -0800

    Add Camera & Player

--- BowlingWithPhysics/Assets/Scenes/InputManager.cs
+++ BowlingWithPhysics/Assets/Scenes/InputManager.cs
@@ -1,13 +1,28 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
+
+
 public class InputManager : MonoBehaviour
 {
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
-    void Update(){
+
+    private void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             OnSpacePressed?.Invoke();
         }
+
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+        OnMove?.Invoke(input);
     }
 }
