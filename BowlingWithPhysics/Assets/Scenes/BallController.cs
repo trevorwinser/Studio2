118273eea6fbb0b2826b7385dcc5041ded8ec9d5
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2025-02-15 13:46:12 -0800

    Add Launch Indicator

--- BowlingWithPhysics/Assets/Scenes/BallController.cs
+++ BowlingWithPhysics/Assets/Scenes/BallController.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
     [SerializeField] private InputManager inputManager;
+    [SerializeField] private Transform launchIndicator;
 
-    // name booleans like a question
     private bool isBallLaunched;
     private Rigidbody ballRB;
 
@@ -15,16 +15,19 @@ public class BallController : MonoBehaviour
     {
         ballRB = GetComponent<Rigidbody>();
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
 
     private void LaunchBall()
     {
-        // now your if check can be framed like a sentence
-        // "if ball is launched, then simply return and do nothing"
         if (isBallLaunched) return;
-        // "now that the ball is not launched, set it to true and launch the ball"
         isBallLaunched = true;
-        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
+        transform.parent = null;
+        ballRB.isKinematic = false;
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
     }
 }
